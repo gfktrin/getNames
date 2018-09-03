d5cddd9829207298e44bedde2c064ed933e1be27
Gabriel Trindade <gfktrin@yahoo.com.br>
2018-09-03 16:46:37 -0300

    limpa dados separando alunos

--- limpaDados.py
+++ limpaDados.py
@@ -6,6 +6,8 @@ with open("alunos.json", "r") as read_file:
 pages = []
 pages_brute_data = []
 brute_data_fragments = []
+fragment_pieces = []
+students = []
 
 for x in range(len(data)):
   pages.append(data[x])
@@ -19,4 +21,17 @@ for x in range(len(pages_brute_data)):
   brute_data = pages_brute_data[x]
   for y in range(len(brute_data)):
     brute_data_fragments.append(brute_data[y])
+
+for x in range(len(brute_data_fragments)):
+  fragment = brute_data_fragments[x]
+  for y in range(len(fragment)):
+    fragment_pieces.append(fragment[y])
+
+for x in range(len(fragment_pieces)):
+  piece = fragment_pieces[x]
+  if(len(piece['text']) > 16):
+    students.append(piece['text'])
+
+print(students)
+print(len(students))
 
